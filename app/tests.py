8a755faddbeff70bce38913b58015169f77b4bca
Dan Davis <dan@danizen.net>
2018-10-28 22:36:17 -0400

    Add a migration that creates the UDF and more tests.

--- app/tests.py
+++ app/tests.py
@@ -6,14 +6,46 @@ from django.db.models import Func, F
 from app.models import *
 
 
-class TestFixturesAndUDF(TestCase):
+class UDFMixin:
+    """
+    This mixin tests a queryset that depends on the User-defined function.
+    """
+    # NOTE: done as a mixin to force many, many tests.
     fixtures = ['sarahbooks']
-
-    def test_fixture(self):
-        sarah_counts = Author.objects.filter(first_name='Sarah').count()
-        self.assertEqual(sarah_counts, 1)
 
     def test_udf(self):
         sarah = Author.objects.annotate(hello=Func(F('first_name'), function='HELLO')).filter(first_name='Sarah').first()
         self.assertEqual(sarah.hello, 'Hello, Sarah!')
+
+
+class TestOne(UDFMixin, TestCase):
+    pass
+
+
+class TestTwo(UDFMixin, TestCase):
+    pass
+
+
+class TestThree(UDFMixin, TestCase):
+    pass
+
+
+class TestFour(UDFMixin, TestCase):
+    pass
+
+
+class TestFive(UDFMixin, TestCase):
+    pass
+
+
+class TestSix(UDFMixin, TestCase):
+    pass
+
+
+class TestSeven(UDFMixin, TestCase):
+    pass
+
+
+class TestEight(UDFMixin, TestCase):
+    pass
 
